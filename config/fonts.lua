565f7e30148e0e91a661a7c8219cbc23e4bf5c77
genmzy <986299679@qq.com>
2024-01-15 13:35:54 +0800

    feat(font): fix windows font config

--- config/fonts.lua
+++ config/fonts.lua
@@ -3,6 +3,7 @@ local platform = require('utils.platform')()
 
 local font_size
 local font
+local line_height = 1.0
 if platform.is_linux then
   font = wezterm.font_with_fallback({
     { family = 'ComicCodeLigatures Nerd Font' },
@@ -10,8 +11,9 @@ if platform.is_linux then
   })
   font_size = 13
 elseif platform.is_win then
-  font = wezterm.font('ComicCodeLigatures NF')
-  font_size = 12
+  line_height = 1.2
+  font = wezterm.font('RecMonoGenmzyUnsimple NF')
+  font_size = 13.5
 else
   font = wezterm.font('ComicCodeLigatures Nerd Font')
   font_size = 12
@@ -22,6 +24,7 @@ return {
   font_size = font_size,
   text_background_opacity = 1.0,
 
+  line_height = line_height,
   --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
   freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
   freetype_render_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
