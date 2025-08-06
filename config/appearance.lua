24f2f3c1eb33fef58b0cc56b51eef6962ce865c2
genmzy <986299679@qq.com>
2025-08-06 11:13:02 +0800

    feat: change opacity

--- config/appearance.lua
+++ config/appearance.lua
@@ -15,7 +15,7 @@ return {
   background = {
     {
       source = { Color = colors.background },
-      opacity = 0.96,
+      opacity = 0.98,
       width = '100%',
       height = '100%',
     },
