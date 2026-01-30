39807c348d147fee9f08169b03685c9cbab9333f
genmzy <986299679@qq.com>
2026-01-30 13:14:20 +0800

    feat: change line height

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,17 +1,16 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')()
-local sk = require('local.local').screen_k
 
 local font_size
 local font
 local line_height = 1.0
 if platform.is_linux then
-  line_height = 0.92
+  line_height = 1.0
   font = wezterm.font_with_fallback({
     -- { family = 'ComicCodeLigatures Nerd Font' },
     { family = 'Maple Mono NF CN', weight = 300 },
   })
-  font_size = (sk == 1 and 15.1 or 15.7)
+  font_size = 15
 elseif platform.is_win then
   line_height = 1.2
   -- font = wezterm.font('RecMonoGenmzyUnsimple NF')
