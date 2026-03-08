22cbf47833292c175b0fec7400f36be8ee033715
genmzy <986299679@qq.com>
2026-03-08 16:33:41 +0800

    feat: change font size

--- config/fonts.lua
+++ config/fonts.lua
@@ -5,13 +5,13 @@ local font_size
 local font
 local line_height = 1.0
 if platform.is_linux then
-  line_height = 1.05
+  line_height = 1.1
   font = wezterm.font_with_fallback({
     { family = 'Ligamononoki NF' },
-    { family = 'LXGW WenKai Mono' },
     { family = 'ComicCodeLigaturesNerdFontComplete Nerd Font' },
+    { family = 'LXGW WenKai Mono' },
   })
-  font_size = 16.7
+  font_size = 15
 elseif platform.is_win then
   line_height = 1.2
   -- font = wezterm.font('RecMonoGenmzyUnsimple NF')
