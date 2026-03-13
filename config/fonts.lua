5a46a1a6fa53321eac02dd904ba65174de44ede9
genmzy <986299679@qq.com>
2026-03-13 09:25:49 +0800

    feat: xenon

--- config/fonts.lua
+++ config/fonts.lua
@@ -7,11 +7,11 @@ local line_height = 1.0
 if platform.is_linux then
   line_height = 1.1
   font = wezterm.font_with_fallback({
-    { family = 'Ligamononoki NF' },
-    { family = 'ComicCodeLigaturesNerdFontComplete Nerd Font' },
+    { family = 'Monaspace Xenon Frozen', weight = 400 },
+    { family = 'LigaSrcPro Nerd Font' },
     { family = 'LXGW WenKai Mono' },
   })
-  font_size = 15
+  font_size = 14
 elseif platform.is_win then
   line_height = 1.2
   -- font = wezterm.font('RecMonoGenmzyUnsimple NF')
